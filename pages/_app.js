3806f163fa37ce8195c817f67f2fbfce16c83fcd
John Hooper <nisse038@gmail.com>
2020-10-25 21:52:04 -0500

    figure out ssr for emotion!!

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
+
+import NextApp from 'next/app'
+import { CacheProvider } from '@emotion/core'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Use only { cache } from 'emotion'. Don't use { css }.
+import { cache } from 'emotion'
+
+// import { globalStyles } from '../shared/styles'
+
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <CacheProvider value={cache}>
+        <Component {...pageProps} />
+      </CacheProvider>
+    )
+  }
 }
 
-export default MyApp
+
+// import '../styles/globals.css'
+// import { CacheProvider } from '@emotion/core'
+// import { cache } from 'emotion'
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <CacheProvider value={cache}>
+//       <Component {...pageProps} />
+//     </CacheProvider>
+//   )
+// }
+
+// export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,84 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { extractCritical } from 'emotion-server'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = extractCritical(initialProps.html)
+    console.log(styles)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style
+            data-emotion-css={styles.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: styles.css }}
+          />
+        </>
+      ),
     }
   }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+// import Document, {Head, Main, NextScript} from 'next/document'
+// import { ServerStyleSheet } from 'styled-components'
+// import { extractCritical } from 'emotion-server'
+
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const page = ctx.renderPage();
+//     const styles = extractCritical(page.html)
+
+//     return {
+//       ...page,
+//       ...styles
+//     }
+//   }
+
+//   constructor(props){
+//     super(props);
+//   }
+// }
+
+
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const sheet = new ServerStyleSheet()
+//     const originalRenderPage = ctx.renderPage
+//     try {
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: (App) => (props) =>
+//             sheet.collectStyles(<App {...props} />),
+//         })
+
+//       const initialProps = await Document.getInitialProps(ctx)
+//       const styles = extractCritical(initialProps.html);
+//       console.log(styles)
+//       return {
+//         ...initialProps,
+//         styles: (
+//           <>
+//             {initialProps.styles}
+//             {sheet.getStyleElement()}
+//           </>
+//         ),
+//       }
+//     } finally {
+//       sheet.seal()
+//     }
+//   }
+// }
